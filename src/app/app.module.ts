21a07bbae93eec5989eb83d7ec21e6511e9fd731
wntitus <wesn.titus@gmail.com>
2018-09-24 09:24:46 -0400

    working on landing page and adding main logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LogoBoxComponent } from './logo-box/logo-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoBoxComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/logo-box/logo-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-box',
+  templateUrl: './logo-box.component.html',
+  styleUrls: ['./logo-box.component.css']
+})
+export class LogoBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
