4013ec8fda727f90de45bf3580b1f31ebbbb62a1
Wesley Titus <wesn.titus@gmail.com>
2018-09-26 09:59:01 -0400

    adding initial login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { LogoBoxComponent } from './logo-box/logo-box.component';
 import { LoginCtaComponent } from './logo-box/login-cta/login-cta.component';
 import { SignupCtaComponent } from './logo-box/signup-cta/signup-cta.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { ErrorNotFoundComponent } from './error-not-found/error-not-found.component';
 import { ReturnBtnComponent } from './error-not-found/return-btn/return-btn.component';
 import { SignupBoxComponent } from './signup-box/signup-box.component';
 import { AuthBoxTitleComponent } from './shared/auth-box-title/auth-box-title.component';
 import { LoginBoxComponent } from './login-box/login-box.component';
 import { HomeBtnComponent } from './shared/home-btn/home-btn.component';
+import { MatInputModule,
+  MatCardModule,
+  MatButtonModule } from '@angular/material';
 
 
 @NgModule({
@@ -29,7 +33,11 @@ import { HomeBtnComponent } from './shared/home-btn/home-btn.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
