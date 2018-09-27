ec6cfc513361f19b581122adadfaaf094d2ccc96
Wesley Titus <wesn.titus@gmail.com>
2018-09-27 10:16:49 -0400

    adding placeholders to dashboard and components

--- src/app/dashboard/study-box/study-topic/study-topic.component.ts
+++ src/app/dashboard/study-box/study-topic/study-topic.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-study-topic',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./study-topic.component.css']
 })
 export class StudyTopicComponent implements OnInit {
+  @Input() topicImage = '';
 
   constructor() { }
 
